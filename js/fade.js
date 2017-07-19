3a51f7697f32c7f07c9b69bc41a26aa9e8e5e265
maxnormansites <maxnormansites@gmail.com>
2017-07-19 11:54:08 -0700

    Update fade.js

--- js/fade.js
+++ js/fade.js
@@ -1,6 +1,8 @@
+$(document).ready(function(){
 $("button").click(function(){
-    $("p").fadeOut();
+    $(fadeOut).fadeOut();
 });
 $("button").click(function(){
-    $("p").fadeIn();
+    $(fadeIn).fadeIn();
+});
 });
